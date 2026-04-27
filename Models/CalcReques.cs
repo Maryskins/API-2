5df7799457d3203867233823e245ed42ed9e95d5
Андрей <alapshin2007@mail.ru>
2026-04-27 11:33:07 +0300

    Add files via upload

---
+++ Models/CalcReques.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+
+namespace CalculatorAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public AppDbContext() { }
+
+        public DbSet<CalcRequest> Calculations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Для миграций
+                optionsBuilder.UseSqlServer(@"Server=507-12\SQLEXPRESS;Database=CalcDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+        }
+    }
+}
